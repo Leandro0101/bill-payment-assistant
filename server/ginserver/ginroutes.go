60157dded9d69b60e3dd0db270eaf66514b9794f
LEANDRO <leandrovieira3306@gmail.com>
2022-11-06 11:05:25 -0300

    chore: add live reload with air

--- server/ginserver/ginroutes.go
+++ server/ginserver/ginroutes.go
@@ -8,8 +8,8 @@ import (
 
 func configRoutes(router *gin.Engine, APP_VERSION string) * gin.Engine {
 	router.GET("/", func (c *gin.Context){
-		c.JSON(http.StatusOK, "HELLO")
+		c.JSON(http.StatusOK, "HELLO2")
 	})
-	
+
 	return router
 }
